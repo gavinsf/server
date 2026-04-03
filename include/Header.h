aabe15fbbbafa02ed50aa28223dd3e595aa02072
gavinsf <gsfoster031@gmail.com>
2026-04-02 18:07:49 -0700

    Overloads == for HeaderPos

--- include/Header.h
+++ include/Header.h
@@ -8,6 +8,12 @@ struct HeaderPos
 {
     size_t key_start, key_len;
     size_t val_start, val_len;
+    bool operator==(const HeaderPos& other) const {
+        return key_start == other.key_start &&
+               key_len   == other.key_len   &&
+               val_start == other.val_start &&
+               val_len   == other.val_len;
+    }
 };
 
 class Header
